ac074afac933c6cc991d62667554da978ef739fe
Bruno Castro <bs8872491@gmail.com>
2022-06-17 15:33:43 -0300

    Criando o modelo Produto e registrando no admin

--- produtos/admin.py
+++ produtos/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from produtos.models import Product
 
-# Register your models here.
+class ProductList(admin.ModelAdmin):
+    list_display = ('name', 'price', 'category', 'available')
+    list_filter = ('category',)
+    list_editable = ('available',)
+
+admin.site.register(Product, ProductList)
--- produtos/models.py
+++ produtos/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.CharField(max_length=30)
+    brand = models.CharField(max_length=30)
+    category = models.CharField(max_length=30)
+    available = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.name
